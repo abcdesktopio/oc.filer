b3659c4676e9292c14053ebf3c7b74a64f435ea4
Matt307082 <matteobeghelli2003@gmail.com>
2026-05-21 11:24:00 +0200

    add ACCEPT_BINARIES_UPLOAD and ACCEPT_COMPRESSED_UPLOAD env variables

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,11 @@
 
 # Read first $POD_IP if not set get from hostname -i ip address
 export CONTAINER_IP_ADDR=${POD_IP:-$(hostname -i)}
+export ACCEPT_BINARIES_UPLOAD=false
+export ACCEPT_COMPRESSED_UPLOAD=false
 echo "Container local ip addr is $CONTAINER_IP_ADDR"
+echo "Accept binaries upload: $ACCEPT_BINARIES_UPLOAD"
+echo "Accept compressed upload: $ACCEPT_COMPRESSED_UPLOAD"
 
 # there is only one process to run
 # we do not need to use supervisord
